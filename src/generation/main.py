eb1d5dbbfb47444e76e283f935619c1d42dd6b7a
wtalioy <2251065331@qq.com>
2025-07-18 11:50:48 +1000

    chore: Update import paths in main.py for module accessibility

--- src/generation/main.py
+++ src/generation/main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 from generation.datasets import RAWDATASET_MAP
 from generation.models import TTS_MODEL_MAP, VC_MODEL_MAP
 import argparse
