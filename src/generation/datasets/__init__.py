cc31cd83dd73e29cab93a043fe85ea11ef15113a
wtalioy <2251065331@qq.com>
2025-07-19 12:39:58 +1000

    feat: Add Podcast generation

--- src/generation/datasets/__init__.py
+++ src/generation/datasets/__init__.py
@@ -1,8 +1,10 @@
 from .base import BaseRawDataset
 from .news import News
+from .podcast import Podcast
 
 RAWDATASET_MAP = {
     "news": News,
+    "podcast": Podcast,
 }
 
 __all__ = ["BaseRawDataset", "RAWDATASET_MAP"]
