54dbcac0e6bbaed36e1b9e4bb55e63fecd4db821
wtalioy <2251065331@qq.com>
2025-07-19 10:50:51 +1000

    feat: Enhance logging

--- src/eval/main.py
+++ src/eval/main.py
@@ -1,9 +1,13 @@
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import argparse
+from loguru import logger
 from eval.baselines import BASELINE_MAP
 from eval.datasets import DATASET_MAP
 
 def main(args):
-    baseline = BASELINE_MAP[args.baseline]
+    baseline = BASELINE_MAP[args.baseline](wav2vec_model_path=args.wav2vec_model_path)
     dataset = DATASET_MAP[args.dataset]()
     dataset.evaluate(baseline, args.metrics)
 
@@ -12,5 +16,11 @@ if __name__ == "__main__":
     parser.add_argument("--baseline", type=str, default="ardetect", help="Name of the baseline", choices=list(BASELINE_MAP.keys()))
     parser.add_argument("--dataset", type=str, default="public", help="Name of the dataset", choices=list(DATASET_MAP.keys()))
     parser.add_argument("--metrics", type=str, nargs="+", default=["eer"], help="Metrics to evaluate", choices=["eer"])
+    parser.add_argument("--wav2vec_model_path", type=str, default="/home/ruimingwang/AR-Detect/cache/extractors/wav2vec2-xls-r-2b", help="Path to the wav2vec model")
     args = parser.parse_args()
+
+    os.makedirs("logs", exist_ok=True)
+    logger.add("logs/eval.log", rotation="100 MB", retention="60 days")
+    logger.info(f"Evaluating {args.baseline} on {args.dataset} with metrics: {args.metrics}")
+
     main(args)
--- src/generation/main.py
+++ src/generation/main.py
@@ -1,9 +1,11 @@
 import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
-from generation.datasets import RAWDATASET_MAP
-from generation.models import TTS_MODEL_MAP, VC_MODEL_MAP
 import argparse
+from loguru import logger
+from generation.models import TTS_MODEL_MAP, VC_MODEL_MAP
+from generation.datasets import RAWDATASET_MAP
+
 
 def main(args):
     raw_dataset = RAWDATASET_MAP[args.dataset](data_dir=args.data_dir)
@@ -17,6 +19,10 @@ if __name__ == "__main__":
     parser.add_argument("--tts_model", type=str, nargs="+", default=["xttsv2", "melotts", "tacotron2", "bark"], help="Name of the TTS model", choices=list(TTS_MODEL_MAP.keys()))
     parser.add_argument("--vc_model", type=str, nargs="+", default=["knnvc", "freevc", "openvoice"], help="Name of the VC model", choices=list(VC_MODEL_MAP.keys()))
     parser.add_argument("--data_dir", type=str, default=None, help="Directory for dataset")
-    
     args = parser.parse_args()
+
+    os.makedirs("logs", exist_ok=True)
+    logger.add("logs/generation.log", rotation="100 MB", retention="60 days")
+    logger.info(f"Generating fake audio data for {args.dataset} with TTS models: {args.tts_model} and VC models: {args.vc_model}")
+
     main(args)
