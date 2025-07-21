b43e7412d187cd98121f2316cf975a33be4c3bca
wtalioy <2251065331@qq.com>
2025-07-21 18:50:42 +1000

    fix: Use dynamic language setting for PhoneCall generation

--- src/generation/datasets/phonecall.py
+++ src/generation/datasets/phonecall.py
@@ -6,6 +6,7 @@ from .base import BaseRawDataset
 class PhoneCall(BaseRawDataset):
     def __init__(self, data_dir=None, split="en", *args, **kwargs):
         super().__init__(os.path.join(data_dir or "data/PhoneCall", split), *args, **kwargs)
+        self.split = split
 
     def generate(self, tts_models: List[BaseTTS], vc_models: List[BaseVC] = [], *args, **kwargs):
-        super().generate(tts_models, vc_models, language="en", use_case="phonecall", *args, **kwargs)
+        super().generate(tts_models, vc_models, language=self.split, use_case="phonecall", *args, **kwargs)
