943187927c742cda29b8e8629fbda20b9cab8c74
wtalioy <2251065331@qq.com>
2025-07-22 17:00:48 +1000

    refactor: Update Audiobook and PhoneCall constructors to accept additional arguments

--- src/eval/datasets/phonecall.py
+++ src/eval/datasets/phonecall.py
@@ -2,6 +2,6 @@ import os
 from .base import BaseDataset
 
 class PhoneCall(BaseDataset):
-    def __init__(self, data_dir=None, split="en"):
+    def __init__(self, data_dir=None, split="en", *args, **kwargs):
         data_dir = os.path.join(data_dir or "data/PhoneCall", split)
         super().__init__(data_dir)
