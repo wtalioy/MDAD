33baea9cf7138cbdbe490a51ec9e70e898d8015a
wtalioy <2251065331@qq.com>
2025-07-22 17:02:03 +1000

    feat: Update default dataset and split parameters in evaluation script

--- src/eval/main.py
+++ src/eval/main.py
@@ -9,14 +9,17 @@ from eval.datasets import DATASET_MAP
 def main(args):
     baseline = BASELINE_MAP[args.baseline](**vars(args))
     dataset = DATASET_MAP[args.dataset](**vars(args))
-    dataset.evaluate(baseline, args.metrics)
+    results = dataset.evaluate(baseline, args.metrics)
+    logger.info("Evaluation results:")
+    for metric, value in results.items():
+        logger.info(f"{metric}: {value}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate baseline on dataset")
     parser.add_argument("--baseline", type=str, default="ardetect", help="Name of the baseline", choices=list(BASELINE_MAP.keys()))
-    parser.add_argument("--dataset", type=str, default="public", help="Name of the dataset", choices=list(DATASET_MAP.keys()))
-    parser.add_argument("--split", type=str, default="en", help="Split of the dataset", choices=["en", "zh-cn"])
-    parser.add_argument("--metrics", type=str, nargs="+", default=["eer"], help="Metrics to evaluate", choices=["eer"])
+    parser.add_argument("--dataset", type=str, default="phonecall", help="Name of the dataset", choices=list(DATASET_MAP.keys()))
+    parser.add_argument("--split", type=str, default="zh-cn", help="Split of the dataset", choices=["en", "zh-cn"])
+    parser.add_argument("--metrics", type=str, nargs="+", default=["eer"], help="Metrics to evaluate")
     parser.add_argument("--data_dir", type=str, default=None, help="Path to the data directory")
     parser.add_argument("--wav2vec_model_path", type=str, default="/home/ruimingwang/AR-Detect/cache/extractors/wav2vec2-xls-r-2b", help="Path to the wav2vec model")
     args = parser.parse_args()
