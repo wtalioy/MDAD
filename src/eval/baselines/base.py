128f39ad857689c9c593982180a4e3f802d5cfdd
wtalioy <2251065331@qq.com>
2025-07-26 16:00:44 +1000

    feat: Separate cross-domain and in-domain evaluation mode

--- src/eval/baselines/base.py
+++ src/eval/baselines/base.py
@@ -1,7 +1,13 @@
-from typing import List
+from typing import List, Optional
 import numpy as np
 
 class Baseline:
+    def __init__(self, device: str = "cuda", **kwargs):
+        self.device = device
+        self.supported_metrics = ["eer"]
 
-    def evaluate(self, data: List[str], labels: np.ndarray, metrics: List[str]) -> dict:
+    def evaluate(self, data: List[str], labels: np.ndarray, metrics: List[str], ckpt_path: Optional[str] = None) -> dict:
+        raise NotImplementedError("This method should be overridden by subclasses.")
+
+    def train(self, train_data: List[str], train_labels: np.ndarray, eval_data: List[str], eval_labels: np.ndarray, dataset_name: str) -> str:
         raise NotImplementedError("This method should be overridden by subclasses.")
--- src/eval/main.py
+++ src/eval/main.py
@@ -1,31 +1,44 @@
-import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 import argparse
 from loguru import logger
-from eval.baselines import BASELINE_MAP
-from eval.datasets import DATASET_MAP
+from baselines import BASELINE_MAP
+from cmad_datasets import DATASET_MAP
 
 def main(args):
+    os.makedirs("logs", exist_ok=True)
+    log_id = logger.add("logs/eval.log", rotation="100 MB", retention="60 days")
+    logger.info(f"Evaluating {args.baseline} on {args.dataset} with metrics: {args.metrics} in {args.mode} mode")
+    
     baseline = BASELINE_MAP[args.baseline](**vars(args))
     dataset = DATASET_MAP[args.dataset](**vars(args))
-    results = dataset.evaluate(baseline, args.metrics)
+
+    if args.mode == "cross-domain":
+        if not args.train_only:
+            results = dataset.evaluate(baseline, args.metrics)
+    elif args.mode == "in-domain":
+        if not args.eval_only:
+            logger.info("Training baseline")
+            logger.remove(log_id)
+            dataset.train(baseline)
+            return
+        if not args.train_only:
+            results = dataset.evaluate(baseline, args.metrics)
+            logger.add("logs/eval.log", rotation="100 MB", retention="60 days")
+
     logger.info("Evaluation results:")
     for metric, value in results.items():
         logger.info(f"{metric}: {value}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Evaluate baseline on dataset")
-    parser.add_argument("--baseline", type=str, default="ardetect", help="Name of the baseline", choices=list(BASELINE_MAP.keys()))
-    parser.add_argument("--dataset", type=str, default="phonecall", help="Name of the dataset", choices=list(DATASET_MAP.keys()))
-    parser.add_argument("--subset", type=str, default="zh-cn", help="Subset of the dataset", choices=["en", "zh-cn"])
+    parser.add_argument("--baseline", type=str, default="aasist", help="Name of the baseline", choices=list(BASELINE_MAP.keys()))
+    parser.add_argument("--dataset", type=str, default="publicfigure", help="Name of the dataset", choices=list(DATASET_MAP.keys()))
+    parser.add_argument("--subset", type=str, default="en", help="Subset of the dataset", choices=["en", "zh-cn"])
+    parser.add_argument("--mode", type=str, default="cross-domain", help="Mode of the evaluation", choices=["cross-domain", "in-domain"])
+    parser.add_argument("--train_only", action="store_true", help="Train the baseline only")
+    parser.add_argument("--eval_only", action="store_true", help="Evaluate the baseline only")
     parser.add_argument("--metrics", type=str, nargs="+", default=["eer"], help="Metrics to evaluate")
     parser.add_argument("--data_dir", type=str, default=None, help="Path to the data directory")
-    parser.add_argument("--wav2vec_model_path", type=str, default="/home/ruimingwang/AR-Detect/cache/extractors/wav2vec2-xls-r-2b", help="Path to the wav2vec model")
     args = parser.parse_args()
-
-    os.makedirs("logs", exist_ok=True)
-    logger.add("logs/eval.log", rotation="100 MB", retention="60 days")
-    logger.info(f"Evaluating {args.baseline} on {args.dataset} with metrics: {args.metrics}")
 
     main(args)
