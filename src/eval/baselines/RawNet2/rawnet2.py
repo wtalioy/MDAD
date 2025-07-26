50d93149f1961e65e428b842682a75e5857bf729
wtalioy <2251065331@qq.com>
2025-07-26 17:50:10 +1000

    feat: Implement RawNet2 model with configuration, training, and evaluation logic

---
+++ src/eval/baselines/RawNet2/rawnet2.py
@@ -0,0 +1,157 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from sklearn.metrics import roc_curve
+from scipy.optimize import brentq
+from scipy.interpolate import interp1d
+from tqdm import tqdm
+from loguru import logger
+from typing import List, Optional
+import numpy as np
+import soundfile as sf
+import os
+
+from baselines.RawNet2.model import RawNet as RawNetModel
+from baselines.RawNet2.parser import get_args
+from baselines.RawNet2.utils import *
+
+from baselines import Baseline
+
+class RawNet2(Baseline):
+    def __init__(self, config: str = "config.yaml", device: str = "cuda", **kwargs):
+        self.args = get_args(config)
+        self.device = device
+        self.default_ckpt = os.path.join(os.path.dirname(__file__), "ckpts", "asvspoof2019_LA.pth")
+        self.model = RawNetModel(self.args.model, device).to(device)
+        
+        self.supported_metrics = ["eer"]
+
+    def _init_train(self):
+        if self.args.multi_gpu:
+            self.model_to_save = self.model
+            self.model = nn.DataParallel(self.model_to_save).to(self.device)
+        else:
+            self.model_to_save = self.model
+        self.model.apply(init_weights)
+        self.criterion = nn.CrossEntropyLoss()
+        params = [
+            {
+                'params': [
+                    param for name, param in self.model.named_parameters()
+                    if 'bn' not in name
+                ]
+            },
+            {
+                'params': [
+                    param for name, param in self.model.named_parameters()
+                    if 'bn' in name
+                ],
+                'weight_decay':
+                0
+            },
+        ]
+        if self.args.optimizer.lower() == 'sgd':
+            self.optimizer = torch.optim.SGD(params, lr=self.args.lr, weight_decay=self.args.wd)
+        elif self.args.optimizer.lower() == 'adam':
+            self.optimizer = torch.optim.Adam(params, lr=self.args.lr, weight_decay=self.args.wd, amsgrad=self.args.amsgrad)
+        else:
+            raise ValueError(f"Optimizer {self.args.optimizer} not supported")
+        self.scheduler = torch.optim.lr_scheduler.LambdaLR(self.optimizer, lr_lambda = lambda step: keras_lr_decay(step))
+
+    def _prepare_loader(self, data: List[str], labels: np.ndarray, batch_size: int = 128, shuffle: bool = True, drop_last: bool = True, num_workers: int = 8):
+        def pad(x, max_len=64600):
+            x_len = x.shape[0]
+            if x_len >= max_len:
+                return x[:max_len]
+            # need to pad
+            num_repeats = int(max_len / x_len) + 1
+            padded_x = np.tile(x, (1, num_repeats))[:, :max_len][0]
+            return padded_x
+
+        class CustomDataset(Dataset):
+            def __init__(self, data):
+                self.paths = data
+                self.labels = labels
+            def __len__(self):
+                return len(self.paths)
+            def __getitem__(self, idx):
+                path = self.paths[idx]
+                X, _ = sf.read(path) 
+                X_pad = pad(X)
+                x_inp= torch.from_numpy(X_pad).float()
+                y = self.labels[idx]
+                return x_inp, y
+
+        dataset = CustomDataset(data)
+        loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+        return loader
+
+    def _train_epoch(self, epoch: int, train_loader: DataLoader):
+        self.model.train()
+        with tqdm(total = len(train_loader), ncols = 70, desc="Training") as pbar:
+            for batch, label in train_loader:
+                batch, label = batch.to(self.device), label.to(self.device)
+                output = self.model(batch, label)
+                cce_loss = self.criterion(output, label)
+                loss = cce_loss
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                pbar.set_description('epoch: %d, cce:%.3f'%(epoch, cce_loss))
+                pbar.update(1)
+                self.scheduler.step()
+
+    @torch.no_grad()
+    def _evaluate_eer(self, eval_loader: DataLoader) -> float:
+        self.model.eval()
+        scores = []
+        labels = []
+        with tqdm(total = len(eval_loader), ncols = 70, desc="Evaluating EER") as pbar:
+            for batch, label in eval_loader:
+                batch = batch.to(self.device)
+                code = self.model(batch)
+                scores.extend(code[:, 1].cpu().numpy().ravel().tolist())
+                labels.extend(label)
+                pbar.update(1)
+        fpr, tpr, _ = roc_curve(labels, scores, pos_label=1)
+        eer = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
+        return float(eer)
+
+    def train(self, train_data: List[str], train_labels: np.ndarray, eval_data: List[str], eval_labels: np.ndarray, dataset_name: str):
+        log_id = logger.add("logs/train.log", rotation="100 MB", retention="60 days")
+        logger.info(f"Training RawNet2 on {dataset_name}")
+        self._init_train()
+        best_eer = 100
+        best_epoch = 0
+        train_loader = self._prepare_loader(train_data, train_labels)
+        eval_loader = self._prepare_loader(eval_data, eval_labels)
+        save_path = os.path.join(os.path.dirname(__file__), "ckpts", f"{dataset_name}_best.pt")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        for epoch in range(self.args.epoch):
+            self._train_epoch(epoch, train_loader)
+            eer = self._evaluate_eer(eval_loader)
+            logger.info(f"Epoch {epoch} EER: {100*eer:.2f}%")
+            if eer < best_eer:
+                best_eer = eer
+                best_epoch = epoch
+                torch.save(self.model_to_save.state_dict(), save_path)
+                logger.info(f"New best EER: {100*best_eer:.2f}% at epoch {epoch}")
+        logger.info(f"Training complete! Best EER: {100*best_eer:.2f}% at epoch {best_epoch}")
+        logger.remove(log_id)
+        self.model = self.model_to_save
+
+    def evaluate(self, data: List[str], labels: np.ndarray, metrics: List[str], in_domain: bool = False, dataset_name: Optional[str] = None) -> dict:
+        if in_domain:
+            self.model.load_state_dict(torch.load(os.path.join(os.path.dirname(__file__), "ckpts", f"{dataset_name}_best.pt")))
+        else:
+            self.model.load_state_dict(torch.load(self.default_ckpt))
+        eval_loader = self._prepare_loader(data, labels, shuffle=False, drop_last=False)
+        
+        results = {}
+        for metric in metrics:
+            if metric not in self.supported_metrics:
+                raise ValueError(f"Unsupported metric: {metric}")
+            func = getattr(self, f"_evaluate_{metric}")
+            metric_rst = func(eval_loader)
+            results[metric] = metric_rst
+        return results
