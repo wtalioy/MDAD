b0f27a9c84fffda1b010cff1461eb7a576e41160
wtalioy <2251065331@qq.com>
2025-07-26 15:58:23 +1000

    refactor: Update baseline imports and expand BASELINE_MAP with new models

--- src/eval/baselines/__init__.py
+++ src/eval/baselines/__init__.py
@@ -1,12 +1,16 @@
 from .base import Baseline
-from .aasist.aasist import AASIST
+from .aasist.aasist import AASIST, AASIST_L
 from .ardetect.ardetect import ARDetect
-from .TSSDNet.tssdnet import TSSDNet
+from .TSSDNet.tssdnet import Res_TSSDNet, Inc_TSSDNet
+from .RawNet2.rawnet2 import RawNet2
 
 BASELINE_MAP = {
     "aasist": AASIST,
+    "aasist-l": AASIST_L,
     "ardetect": ARDetect,
-    "tssdnet": TSSDNet,
+    "res-tssdnet": Res_TSSDNet,
+    "inc-tssdnet": Inc_TSSDNet,
+    "rawnet2": RawNet2,
 }
 
 __all__ = ["Baseline", "BASELINE_MAP"]
