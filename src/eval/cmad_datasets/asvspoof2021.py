84dc11d627e15ed9da4e57adaf5e24afa2332ac8
wtalioy <2251065331@qq.com>
2025-08-06 10:40:38 +1000

    refactor: Simplify dataset loading in ASVspoof2021

--- src/eval/cmad_datasets/asvspoof2021.py
+++ src/eval/cmad_datasets/asvspoof2021.py
@@ -1,7 +1,5 @@
 from typing import List
 import os
-import json
-import soundfile as sf
 import numpy as np
 from datasets import load_dataset
 from tqdm import tqdm
@@ -17,34 +15,18 @@ class ASVspoof2021(BaseDataset):
         self.name = f"ASVspoof2021_{subset}"
 
     def _load_meta(self):
-        paths_meta = os.path.join(self.data_dir, "audio_paths.json")
-        labels_meta = os.path.join(self.data_dir, "labels.json")
-        if not os.path.exists(paths_meta) or not os.path.exists(labels_meta):
-            os.makedirs(self.data_dir, exist_ok=True)
-            file_paths = []
-            labels = []
-            hf_dataset = load_dataset(f"MoaazTalab/ASVspoof_2021_{self.subset}_Balanced_Normalized", split="test")
-            for i, item in enumerate(tqdm(hf_dataset, desc="Loading dataset")):
-                try:
-                    wav, sr = item["audio"]["array"], item["audio"]["sampling_rate"]
-                    audio_path = os.path.join(self.data_dir, f"{i}.wav")
-                    sf.write(audio_path, wav, sr)
-                    label = Label.fake if item["label"] == 0 else Label.real
-                    file_paths.append(audio_path)
-                    labels.append(label)
-                except Exception as e:
-                    print(f"Error loading item {i}: {e}")
-                    continue
-            with open(paths_meta, "w", encoding="utf-8") as f:
-                json.dump(file_paths, f)
-            with open(labels_meta, "w", encoding="utf-8") as f:
-                json.dump(labels, f)
-        else:
-            with open(paths_meta, "r", encoding="utf-8") as f:
-                file_paths = json.load(f)
-            with open(labels_meta, "r", encoding="utf-8") as f:
-                labels = json.load(f)
-        return file_paths, np.array(labels)
+        hf_dataset = load_dataset(f"MoaazTalab/ASVspoof_2021_{self.subset}_Balanced_Normalized", split="test")
+        data = []
+        labels = []
+        for item in tqdm(hf_dataset, desc="Loading dataset"):
+            try:
+                audio, _ = item["audio"]["array"], item["audio"]["sampling_rate"]
+                data.append(audio)
+                labels.append(Label.fake if item["label"] == 0 else Label.real)
+            except Exception as e:
+                print(f"Error loading item {item}: {e}")
+                continue
+        return data, np.array(labels)
 
     def train(self, baseline: Baseline) -> str:
         raise NotImplementedError("Training is not supported for ASVspoof2021")
