91fa176a86602ee0993f04c4947bd85b96670a93
WangRuiming <2251065331@qq.com>
2025-09-08 03:02:03 +0000

    fix: Fix ViTS cython setup

--- src/generation/models/tts/vits/monotonic_align/setup.py
+++ src/generation/models/tts/vits/monotonic_align/setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy
 
+ext_modules = [
+    Extension(
+        "core",
+        ["core.pyx"],
+        include_dirs=[numpy.get_include()]
+    )
+]
+
 setup(
-  name = 'monotonic_align',
-  ext_modules = cythonize("core.pyx"),
-  include_dirs=[numpy.get_include()]
+    name='monotonic_align',
+    ext_modules=cythonize(ext_modules),
 )
